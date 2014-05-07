c2821c8e5c54529e422466a8c5f8b9f2e1a76b14
Nick Campbell <ncampbell@yammer-inc.com>
2014-05-07 15:46:59 +0100

    First commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+ruby '2.1.1'
+
+gem 'sinatra'
+gem 'oauth2'
+gem 'puma'
+
+group :test do
+  gem 'guard-minitest'
+  gem 'minitest'
+end
+
+group :development do
+  gem 'dotenv'
+  gem 'guard'
+  gem 'pry-debugger'
+  gem 'shotgun'
+end
---
+++ application.rb
@@ -0,0 +1,46 @@
+require 'dotenv'
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
+Dotenv.load
+
+class Tester < Sinatra::Base
+
+  RESOURCE = 'https://graph.windows.net'
+  REDIRECT_URI = 'http://localhost:4848/consume'
+
+  def client
+    OAuth2::Client.new                ENV['CLIENT_ID'],
+                                      ENV['CLIENT_SECRET'],
+                       site:          ENV['LOGIN_SITE'],
+                       token_url:     ENV['TOKEN_URL'],
+                       authorize_url: ENV['AUTHORIZE_URL']
+
+  end
+
+  def auth_code
+    client.auth_code
+  end
+
+  get '/' do
+    '<a href="/login">Login</a>'
+  end
+
+  get '/login' do
+    redirect auth_code.authorize_url redirect_uri: REDIRECT_URI,
+                                     resource:     RESOURCE
+  end
+
+  get '/consume' do
+    if params[:code]
+      token = auth_code.get_token params[:code], redirect_uri: REDIRECT_URI
+      token.get('https://graph.windows.net/me?api-version=2013-04-05')
+    end
+    %Q{
+      Logged in with token:
+
+        #{token}
+    }
+  end
+
+end
